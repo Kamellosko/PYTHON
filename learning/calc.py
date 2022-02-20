240af05097005a47f610e65d0df8602407675f33
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 11:30:40 +0100

    master

--- learning/calc.py
+++ learning/calc.py
@@ -1,8 +1,8 @@
+from tokenize import Double
 from unittest import result
 
 
 num1 =input("Enter first number: ")
 num2 =input("Enter second number: ")
-result = int(num1) + int(num2)
+result = float(num1) + float(num2)
 print(result)
-7
