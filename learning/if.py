c10f189d40451a432295492734d6fdf585e4442f
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 13:23:45 +0100

    master

---
+++ learning/if.py
@@ -0,0 +1,14 @@
+is_male = False
+is_tall = False
+
+if is_male or is_tall:
+    print("You are a male or tall or both ")
+else:
+    print("You are not a male nor tall :(")
+
+print("\n")
+
+if is_male and is_tall:
+    print("You are a tall male ")
+else:
+    print("You are not a male or you are not tall :(")
