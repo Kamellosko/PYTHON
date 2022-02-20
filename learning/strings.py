9e1228d02d3095a792cc32e9ffef115e98bebc96
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 11:02:35 +0100

    master

---
+++ learning/strings.py
@@ -0,0 +1,13 @@
+print("prints\naa") 
+
+phrase = "learning this"
+
+print(phrase.upper().isupper()) 
+
+print(len(phrase))
+
+print(phrase[0])
+
+print(phrase.index("ing"))
+
+print(phrase.replace("this", "that"))
---
+++ learning/variables.py
@@ -0,0 +1,5 @@
+character_name ="Carl"
+character_age = "50"
+print("my name is " + character_name)
+print("I am " + character_age)
+print("But im not feeling like "+character_age)
