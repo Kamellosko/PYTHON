b2d69dfb36028d992a4dbaba8fd759525be04063
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 15:15:23 +0100

    master

--- learning/if.py
+++ learning/if.py
@@ -5,15 +5,14 @@ if is_male and is_tall:
     print("You are a male or tall or both ")
 elif is_male and not(is_tall):
     print("You are a male and also you are a small human being")
-elif not(is_male) and not(is_tall):
-    print("Tall woman")
-
+elif not(is_male) and (is_tall):
+    print("You are tall woman")
 else:
-    print("You are not a male nor tall :(")
+    print("You are small woman")
 
 print("\n")
 
-if is_male and is_tall:
-    print("You are a tall male ")
+if is_male or is_tall:
+    print("You are a male or you are just tall ")
 else:
     print("You are not a male or you are not tall :(")
