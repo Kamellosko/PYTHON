d58b9361f89e7bb690472949be8ec438b1ef4716
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 15:25:23 +0100

    master

---
+++ learning/better_calc.py
@@ -0,0 +1,17 @@
+from ast import operator
+
+
+num1= float(input("Enter first number: "))
+operator = (input("Enter  operator: "))
+num2= float(input("Enter second number: "))
+
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+else:
+    print("Invalid operator")
