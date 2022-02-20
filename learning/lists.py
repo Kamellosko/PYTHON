b4d3a358bc4eb0ed717596ea054a0d5a29566181
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 11:56:22 +0100

    master

---
+++ learning/lists.py
@@ -0,0 +1,20 @@
+lucky_numbers = [4,6,1,82,14]
+friends = ["Denis","Dawidek","Grubasek","Kolejny","Pan"]
+friends.extend(lucky_numbers)
+friends.append("Boniek")
+friends.insert(1, "Tede")
+friends.remove("Dawidek")
+
+print(friends)
+
+print("\n")
+
+print(friends.index("Dawidek"))
+
+friends[0] = "Benis"
+print(friends[0])
+print("\n")
+print(friends[-2])
+print("\n")
+print(friends[1:4])
+
