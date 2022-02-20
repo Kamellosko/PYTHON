cb7e4fb53235cd80413a476fd755bff5de271af9
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 12:58:24 +0100

    master

--- learning/functions.py
+++ learning/functions.py
@@ -1,7 +1,14 @@
 from subprocess import call
+from unittest import result
 
 
 def welcome(name, age):
     print("Hello " + name +" you are "+str(age))
 
 welcome("Kamil", 17)
+
+
+def cube(number):
+    return number*number*number
+result= cube(4)
+print(result)
