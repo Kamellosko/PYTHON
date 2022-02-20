f70728b24ec8886eca5dfea7284b13245dffbc43
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 11:16:15 +0100

    master

--- learning/numbers.py
+++ learning/numbers.py
@@ -1,3 +1,4 @@
+from math import *
 print(3 + 4.6)
 print(3-4)
 print(3+3*5)
@@ -6,3 +7,27 @@ print(10)
 
 variable = 30
 print(str(variable) +" number thirty ")
+
+number = -40
+print(abs(number))
+
+print("\n 10^3")
+print(pow(10, 3))
+
+print("\nhigher number")
+print(max(410, 617))
+
+print("\nlower number")
+print(min(410, 617))
+
+print("\n rounding")
+print(round(5.3))
+
+print("\nfloor")
+print(floor(6.3))
+
+print("\nceiling")
+print(ceil(5.1))
+
+print(sqrt(121))
+
