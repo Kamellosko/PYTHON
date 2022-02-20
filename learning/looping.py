6dc355fbfc04f935d82aa8c21b7b469b2f74cbc9
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 16:36:14 +0100

    master

---
+++ learning/looping.py
@@ -0,0 +1,18 @@
+friends=["Adam","Tere","Perry"]
+
+for friend in friends:
+    print(friend)
+
+print("\n")
+
+for number in range(30):
+    if number== 0 :
+        print("First")
+    else:
+        print("Not first")
+
+print("\n")
+
+for number in range(30):
+    print(number)
+    
