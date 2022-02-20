afbc1e62c22c1b330a94c6892c2ceec2a25334a0
Kamellosko <rabit11.kg@gmail.com>
2022-02-20 15:55:20 +0100

    master

---
+++ learning/dictionary.py
@@ -0,0 +1,15 @@
+monthconv = {
+    "Jan":"January",
+    "Feb":"February",
+    "Mar":"March",
+    "Apr":"April",
+    "May":"May",
+    "Jun":"June",
+    "Jul":"July",
+    "Aug":"August",
+    "Sep":"September",
+    "Oct":"October",
+    "Nov":"November",
+    "Dec":"December",
+}
+print(monthconv.get("Jan", "Wrong Key"))
